7f6ca963f8347fee98c8a04ab1e6f05f733e2f0d
egaeuni <gaeun3619@gmail.com>
2024-11-30 23:58:15 +0900

    fect: news 이미지 여러개 업로드

--- news/models.py
+++ news/models.py
@@ -17,3 +17,19 @@ class News(models.Model):
 
     def __str__(self):
         return self.title
+
+    def add_image(self, image):
+        if self.images.count() >= 8:
+            raise ValidationError("최대 8개의 이미지만 업로드할 수 있습니다.")
+        return self.images.create(image=image)
+
+    @property
+    def image_urls(self):
+        return [image.image.url for image in self.images.all()]
+
+class NewsImage(models.Model):
+    news = models.ForeignKey(News, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='upload_filepath', blank=True)
+
+    def __str__(self):
+        return f"Image for {self.news.title}"
--- news/views.py
+++ news/views.py
@@ -6,49 +6,40 @@ from .serializers import NewsSerializer, NewsCreateUpdateSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework.parsers import MultiPartParser, FormParser
 
 class NewsViewSet(viewsets.ModelViewSet):
     queryset = News.objects.all()
     permission_classes = [IsManagerOrReadOnly]
+    parser_classes = [MultiPartParser, FormParser]
 
     def get_serializer_class(self):
         if self.action in ['list', 'retrieve']:
             return NewsSerializer
         return NewsCreateUpdateSerializer
 
-    def get_queryset(self):
-        return News.objects.all()
-
-    def list(self, request, pk=None):
-        serializer = self.get_serializer(self.get_queryset(), many=True)
-        return Response({"message":"뉴스 조회에 성공하였습니다.", "result":serializer.data}, status=status.HTTP_200_OK)
-
     def create(self, request):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(author=request.user)
-            message = "뉴스 등록에 성공하였습니다."
-            return Response({"message": message, "result": serializer.data}, status=status.HTTP_201_CREATED)
+            news = serializer.save(author=request.user)
+            return Response({
+                "message": "뉴스 등록에 성공하였습니다.",
+                "result": NewsSerializer(news).data
+            }, status=status.HTTP_201_CREATED)
         return Response({"message":"뉴스 생성에 실패하였습니다.", "result":serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
     def update(self, request, pk=None):
-        news = get_object_or_404(News, pk=pk)
-        serializer = self.get_serializer(news, data=request.data)
+        news = self.get_object()
+        serializer = self.get_serializer(news, data=request.data, partial=True)
         if serializer.is_valid():
-            serializer.save()
-            message = "뉴스 수정에 성공했습니다."
-            return Response({"message": message, "result": serializer.data}, status=status.HTTP_200_OK)
+            news = serializer.save()
+            return Response({
+                "message": "뉴스 수정에 성공했습니다.",
+                "result": NewsSerializer(news).data
+            }, status=status.HTTP_200_OK)
         return Response({"message": "뉴스 수정에 실패했습니다.", "result": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-    
-    def destroy(self, request, username=None, pk=None):
+
+    def destroy(self, request, pk=None):
         news = get_object_or_404(News, pk=pk)
         news.delete()
         return Response({"message": "뉴스 삭제에 성공했습니다."}, status=status.HTTP_200_OK)
-
-    def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
-        return Response(
-            {"message": "뉴스 조회에 성공하였습니다.", "result": serializer.data},
-            status=status.HTTP_200_OK
-        )
